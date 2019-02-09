2b01c5a8a042c3ae72b2db45e3dbd9d7aa219389
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-10 04:11:01 +0800

    Add code stubbing for Carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,53 @@
 class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel
+        this.buttons = carousel.querySelectorAll('div')
+        this.leftButton = carousel.querySelector('.left-button')
+        this.rightButton = carousel.querySelector('.right-button')
+        
 
+        this.leftButton.addEventListener('click', () => this.goBack())
+        this.rightButton.addEventListener('click', () => this.goForward())
+    }
+
+    goForward () {
+        
+        this.images = carousel.querySelectorAll('img').forEach((image, index) => {
+            let displayedImageIndex = carousel.querySelectorAll('.img').filter((image, index) => {
+                image.style.display === 'block'
+                return index
+            })
+
+        })
+
+    }
+
+    goBack () {
+
+    }
+    
 }
 
-let carousel = document.querySelector();
+// class CarouselImage {
+//     constructor(image) {
+//         this.image = image
+//     }
+
+//     displayImage() {
+//         this.image.style.display = 'block'
+//     }    
+// }
+
+let carousel = document.querySelector('.carousel')
+// console.log(carousel)
+carousel.querySelector('img').style.display = 'block'
+let displayedImageIndex = carousel.querySelectorAll('img')
+
+console.log(displayedImageIndex)
+
+let imageDisplayStatus =  window.getComputedStyle(carousel.querySelector('img'), 'img:last-child').display
+//console.log(imageDisplayStatus)
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
